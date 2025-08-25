08042c744c82fed33477befb13a53f842ff36688
harman-91 <134057473+harman-91@users.noreply.github.com>
2025-08-25 17:19:46 +0530

    update

--- src/services/citiesServices.js
+++ src/services/citiesServices.js
@@ -4,10 +4,11 @@ let BASE_URL = process.env.NEXT_PUBLIC_DOMAIN_URL;
 
 exports.epaperSearchCities = async ({ domain  }) => {
   try {
-    console.log("domain-",`${BASE_URL}${domain}/cities`)
+    console.error("domain-",`${BASE_URL}${domain}/cities`)
     const response = await axios.get(`${BASE_URL}${domain}/cities`);
     return response?.data?.data;
   } catch (error) {
+    console.error("--",error)
     return null;
   }
 };
